97c55263509a28400180d04da56dd988951651d1
YG-paaleee <202380099@psu.palawan.edu.ph>
2025-09-11 02:50:21 +0800

    applied List, Create, Update, Delete View to OrgMember

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,16 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    #ORGANIZATION
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    #ORGMEMBER
+    path('OrgMember_List', OrgMemberList.as_view(), name='OrgMember-list'),
+    path('OrgMember_List/add', OrgMemberCreateView.as_view(), name='OrgMember-add'),
+    path('OrgMember_List/<pk>',OrgMemberUpdateView.as_view(), name='OrgMember-update'),
+    path('OrgMember_List/<pk>/delete', OrgMemberDeleteView.as_view(), name='OrgMember-delete'),
 ]
 
 
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
-from studentorg.forms import OrganizationForm
+from studentorg.forms import OrganizationForm, OrgMemberForm
 from django.urls import reverse_lazy
-from studentorg.models import Organization
+from studentorg.models import Organization, OrgMember, Student, College, Program
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
 
+#ORGANIZATION
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
@@ -32,3 +33,27 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+#ORG MEMBER
+class OrgMemberList(ListView):
+    model = OrgMember
+    context_object_name = 'orgmember'
+    template_name = 'OrgMember_list.html'
+    paginate_by = 5
+
+class OrgMemberCreateView(CreateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'OrgMemberform.html'
+    success_url = reverse_lazy('OrgMember-list')
+
+class OrgMemberUpdateView(UpdateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'OrgMemberform.html'
+    success_url = reverse_lazy('OrgMember-list')
+
+class OrgMemberDeleteView(DeleteView):  
+    model = OrgMember
+    template_name = 'OrgMember_del.html'
+    success_url = reverse_lazy('OrgMember-list')
